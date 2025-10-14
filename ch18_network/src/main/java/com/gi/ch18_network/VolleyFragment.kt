f5073518b967c5d0f5c809e5f200ab10dd27d0d3
onCrown <wns2636@gmail.com>
2025-10-14 17:14:18 +0900

    ch18_network

---
+++ ch18_network/src/main/java/com/gi/ch18_network/VolleyFragment.kt
@@ -0,0 +1,74 @@
+package com.gi.ch18_network
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.gi.ch18_network.databinding.FragmentVolleyBinding
+import com.gi.ch18_network.model.ItemModel
+import com.gi.ch18_network.recycler.MyAdapter
+import org.json.JSONObject
+import com.android.volley.Request
+import com.android.volley.Response
+
+
+class VolleyFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentVolleyBinding.inflate(inflater, container, false)
+
+        //add...................
+        val url = MyApplication.BASE_URL+"/v2/everything?q="+
+                "${MyApplication.QUERY}&apiKey="+
+                "${MyApplication.API_KEY}&page=1&pageSize=5"
+
+        val queue = Volley.newRequestQueue(activity)
+
+        val jsonRequest = object : JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener<JSONObject> { response ->
+                val jsonArray = response.getJSONArray("articles")
+                val mutableList = mutableListOf<ItemModel>()
+                for(i in 0 until jsonArray.length()){
+                    ItemModel().run {
+                        val article = jsonArray.getJSONObject(i)
+                        author = article.getString("author")
+                        title = article.getString("title")
+                        description = article.getString("description")
+                        urlToImage = article.getString("urlToImage")
+                        publishedAt = article.getString("publishedAt")
+                        mutableList.add(this)
+                    }
+                }
+                binding.volleyRecyclerView.layoutManager = LinearLayoutManager(activity)
+                binding.volleyRecyclerView.adapter = MyAdapter(activity as Context, mutableList)
+            },
+            Response.ErrorListener { error ->
+                println("error..... $error")
+            }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                val map = mutableMapOf<String, String>(
+                    "User-agent" to MyApplication.USER_AGENT
+                )
+                return map
+            }
+        }
+
+        queue.add(jsonRequest)
+
+        return binding.root
+    }
+
+}
---
+++ ch18_network/src/main/java/com/gi/ch18_network/model/PageListModel.kt
@@ -0,0 +1,5 @@
+package com.gi.ch18_network.model
+
+class PageListModel {
+    var articles: MutableList<ItemModel>? = null
+}
